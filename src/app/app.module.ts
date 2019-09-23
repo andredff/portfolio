3708f8668edfa4cde0048fd630958d5b3a3022b8
Andre Souza <andre.souza@solucioneonline.com.br>
2019-09-22 23:03:06 -0300

    installing bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
+import { ROUTES } from './routes/app.routing';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './home/landing/landing.component';
+import { TerminalComponent } from './components/terminal/terminal.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    TerminalComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(ROUTES),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/routes/app.routing.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+
+import { LandingComponent } from '../home';
+
+
+export const ROUTES: Routes = [
+  { path: '', component: LandingComponent },
+];
+
